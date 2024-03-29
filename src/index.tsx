b8f68207ded3cc7d2234d028c0cb2078e9a8c941
Mathias Lundin <mathias.lundin87@gmail.com>
2024-03-29 19:57:07 +0100

    use jsx renderer for layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,37 @@
-import 'dotenv/config';
 import { serve } from '@hono/node-server';
+import 'dotenv/config';
 import { Hono } from 'hono';
+import { jsxRenderer } from 'hono/jsx-renderer';
 import counter from './api/counter';
 import Home from './pages/Home';
 
 const app = new Hono();
 
-app.get('/', (c) => c.html(<Home />));
+app.get(
+	'*',
+	jsxRenderer(({ children }) => {
+		return (
+			<html lang="en">
+				<head>
+					<meta charset="UTF-8" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
+					<title>Hono-HTMX</title>
+					<script
+						src="https://unpkg.com/htmx.org@1.9.10"
+						integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
+						crossorigin="anonymous"
+					></script>
+				</head>
+				<body>{children}</body>
+			</html>
+		);
+	})
+);
+
+app.get('/', (c) => c.render(<Home />));
 
 app.route('/api', counter);
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'hono/jsx';
-import Layout from '../layouts';
 import { getLatestCount } from '../functions/counts';
 
 const Home: FC = async () => {
 	const count = await getLatestCount();
 
 	return (
-		<Layout>
+		<>
 			<h1>Hello Hono-HTMX!</h1>
 			<h2>
 				Count: <span id="count">{count?.count ?? 0}</span>
@@ -17,7 +16,7 @@ const Home: FC = async () => {
 			<button hx-post="/api/decrement" hx-swap="none">
 				Decrement
 			</button>
-		</Layout>
+		</>
 	);
 };
 
